b20e790119c72b35158904010ddeb1d581d2fce4
tjuacalla123 <ftjuacal@ucsd.edu>
2021-01-21 12:45:56 -0800

    Done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,29 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Ask your own heart.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").fadeToggle("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+var count = 0;
+function projectClick(e) {
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+    var containingProject = $(this).closest(".project");
+
+    var desc = $(containingProject).find(".project-description")
+    
+    if (desc.length == 0) {
+    	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    }
+    else {
+    	$(".project-description").fadeOut();
+    }
+    
 }
